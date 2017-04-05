e3ac6f203d81480bb134e3c36801bd20ed824f31
Anirudh Sanjeev <skyronic@gmail.com>
2017-04-05 16:37:04 +0530

    initial checkin

---
+++ src/CookieServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Skyronic\Cookie;
+
+use Illuminate\Support\ServiceProvider;
+use Skyronic\Cookie\BakeCommand;
+
+class CookieServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+
+    public function boot () {
+        if ($this->app->runningInConsole ()) {
+            $this->commands([
+            ]);
+        }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+    }
+}
